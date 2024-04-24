ed1f889d1a5596ccbe741cb3ad18a7941a6d113b
Jake Hebert <jacobshebert@gmail.com>
2024-04-23 21:47:35 -0500

    Styling updates for messaging

--- app/assistant/[assistantId]/assistant.tsx
+++ app/assistant/[assistantId]/assistant.tsx
@@ -2,13 +2,15 @@
 
 import { Message as MessageType, useAssistant } from 'ai/react'
 import { Message } from './message'
+import { cn } from '@/lib/utils'
 
 export default function Chat({ assistantId }) {
   const { status, error, messages, input, submitMessage, handleInputChange } =
     useAssistant({ api: `/api/assistant-stream/${assistantId}` })
 
+  const pending = status === 'in_progress'
   return (
-    <div className="stretch mx-auto flex w-full max-w-xl flex-col py-2">
+    <div className="stretch mx-auto flex w-full flex-col gap-4 py-2">
       {error && <div className="text-red-500">{`${error}`}</div>}
       {messages.map((m: MessageType) => (
         <>
@@ -16,19 +18,22 @@ export default function Chat({ assistantId }) {
         </>
       ))}
 
-      {status === 'in_progress' && (
-        <div className="mb-8 h-8 w-full max-w-md animate-pulse rounded-lg bg-gray-300 p-2 dark:bg-gray-600" />
-      )}
-
       <form
         onSubmit={submitMessage}
-        className="fixed bottom-8 w-full max-w-md self-center rounded border border-gray-300 p-2 shadow-xl"
+        className="fixed bottom-4 w-full max-w-md self-center shadow-xl"
       >
         <input
-          className="w-full"
-          disabled={status !== 'awaiting_message'}
+          className={cn(
+            'w-full rounded border border-gray-300 bg-card p-2',
+            pending && 'animate-pulse bg-muted-foreground'
+          )}
+          disabled={pending}
           value={input}
-          placeholder="What kind of questions can I ask?"
+          placeholder={
+            pending
+              ? 'Looking for the answer...'
+              : 'What kind of questions can I ask?'
+          }
           onChange={handleInputChange}
         />
       </form>
--- app/assistant/[assistantId]/message.tsx
+++ app/assistant/[assistantId]/message.tsx
@@ -4,39 +4,65 @@ import { Markdown } from './markdown'
 import { Message as MessageType } from 'ai/react'
 
 const chatVariants = tv({
-  base: `max-w-sm rounded-lg px-3 py-2`,
+  slots: {
+    container: 'max-w-sm self-start rounded-lg bg-gray-900 px-3 py-2',
+    label: 'font-semibold',
+    message: 'prose',
+    data: 'hidden'
+  },
   variants: {
     role: {
-      assistant: `prose-dark self-start bg-gray-900 text-gray-100`,
-      user: `prose self-end border-2 bg-gray-100 text-gray-900`,
-      system: `prose-dark self-start bg-gray-900 text-red-500`,
-      function: `prose-dark self-start bg-gray-900 text-blue-500`,
-      data: `prose-dark self-start bg-gray-900 text-orange-500`,
-      tool: `prose-dark self-start bg-gray-900 text-purple-500`
+      assistant: {
+        container: `text-gray-100`,
+        message: 'prose-invert'
+      },
+      user: {
+        container: `self-end border-2 bg-gray-100 text-end text-gray-900`,
+        message: '',
+        label: 'hidden'
+      },
+      system: {
+        container: `text-red-500`,
+        message: ''
+      },
+      function: {
+        container: ` text-blue-500`,
+        message: ''
+      },
+      data: {
+        container: `text-orange-500`,
+        message: 'hidden',
+        data: 'flex'
+      },
+      tool: {
+        container: `text-purple-500`,
+        message: ''
+      }
     }
   }
 })
 
 export const Message = ({ content, role, data }: MessageType) => {
+  const styles = chatVariants({
+    role
+  })
+
   return (
-    <div
-      className={chatVariants({
-        role
-      })}
-    >
-      <strong>{`${role}`}</strong>
-      {role === 'data' ? (
-        <>
+    <div className={styles.container()}>
+      <p className={styles.label()}>{`${role}`}</p>
+
+      {data && (
+        <div className={styles.data()}>
           {/* here you would provide a custom display for your app-specific data:*/}
-          {(data as any).description}
+          (data as any).description
           <br />
           <pre className={'bg-gray-200'}>{JSON.stringify(data, null, 2)}</pre>
-        </>
-      ) : (
-        <div className="whitespace-pre-wrap">
-          <Markdown markdown={content} />
         </div>
       )}
+
+      <div className={styles.message()}>
+        <Markdown markdown={content} />
+      </div>
     </div>
   )
 }
